7adb7c038eed7faa36065b3f2cd32ae662173638
CerritoCode0101 <CaseyCerrito@gmail.com>
2021-02-11 16:51:48 -0600

    deploy

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // require your server and launch it
+const dotenv = require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const port = process.env.PORT || 4000
 const server = require('./api/server');
 
-const PORT = 4000
+const app = express()
+app.use(cors())
+app.use(express.json())
 
-server.listen(PORT, () =>{
-    console.log('\n* Server Running on http://localhost4000 &\n')
+server.listen(port, () =>{
+    console.log(`\n* Server Running on http://localhost${port} *\n`)
 })
 
